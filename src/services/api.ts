f1b3f1e32faa76a60f6b0174457c30e6d8315ba6
upuna keerawella <upuna.keerawella_ne@ntucguest.com>
2025-01-23 16:54:36 +0530

    API url config name fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_PORT;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const analyzePage = async (url: string) => {
     const response = await axios.get(`${API_BASE_URL}/analyze`, {
