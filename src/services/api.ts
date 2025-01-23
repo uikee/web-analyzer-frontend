065ea5eed6a95944f588f765e198b62ac753f8b5
upuna keerawella <upuna.keerawella_ne@ntucguest.com>
2025-01-23 16:52:18 +0530

    API url config update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_PORT = process.env.REACT_APP_API_PORT || "8080";
-const API_BASE_URL = `http://localhost:${API_PORT}`;
+const API_BASE_URL = process.env.REACT_APP_API_PORT;
 
 export const analyzePage = async (url: string) => {
     const response = await axios.get(`${API_BASE_URL}/analyze`, {
